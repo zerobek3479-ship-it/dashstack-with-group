be2b9909899bd9f90517e649788c8b0cee4749ce
zerobek3479-ship-it <zerobek3479@gmail.com>
2026-03-25 15:30:52 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import DashStackOne from './abbos/DashStackOne'
+import Pricing from './abbos/Pricing'
+import Calendar from './abbos/Calendar'
+import ToDo from './abbos/ToDo'
+import Contact from './abbos/Contact'
+import Invoice from './abbos/Invoice'
+import UIElements from './abbos/UIElements'
+import Team from './abbos/Team'
+import Table from './abbos/Table'
+import Profile from './components/Profile'
+import Settings from './components/Settings'
+import Reports from './pages/Reports'
+
 import { navItems, pageItems, stats } from './abbos/data/abbosDashStackData'
 
 const dashboardData = {
@@ -11,15 +23,18 @@ const dashboardData = {
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<DashStackOne activeTab="dashboard" dashboardData={dashboardData} />} />
-      <Route path="/pricing" element={<DashStackOne activeTab="Pricing" dashboardData={dashboardData} />} />
-      <Route path="/calendar" element={<DashStackOne activeTab="Calendar" dashboardData={dashboardData} />} />
-      <Route path="/to-do" element={<DashStackOne activeTab="To-Do" dashboardData={dashboardData} />} />
-      <Route path="/contact" element={<DashStackOne activeTab="Contact" dashboardData={dashboardData} />} />
-      <Route path="/invoice" element={<DashStackOne activeTab="Invoice" dashboardData={dashboardData} />} />
-      <Route path="/ui-elements" element={<DashStackOne activeTab="UI Elements" dashboardData={dashboardData} />} />
-      <Route path="/team" element={<DashStackOne activeTab="Team" dashboardData={dashboardData} />} />
-      <Route path="/table" element={<DashStackOne activeTab="Table" dashboardData={dashboardData} />} />
+      <Route path="/" element={<DashStackOne dashboardData={dashboardData} />} />
+      <Route path="/pricing" element={<Pricing dashboardData={dashboardData} />} />
+      <Route path="/calendar" element={<Calendar dashboardData={dashboardData} />} />
+      <Route path="/to-do" element={<ToDo dashboardData={dashboardData} />} />
+      <Route path="/contact" element={<Contact dashboardData={dashboardData} />} />
+      <Route path="/invoice" element={<Invoice dashboardData={dashboardData} />} />
+      <Route path="/ui-elements" element={<UIElements dashboardData={dashboardData} />} />
+      <Route path="/team" element={<Team dashboardData={dashboardData} />} />
+      <Route path="/table" element={<Table dashboardData={dashboardData} />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/reports" element={<Reports />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
